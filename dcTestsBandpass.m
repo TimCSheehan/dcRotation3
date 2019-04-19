bcc0b37cbb048952d8aafcd65d5a4b9dadcbc907
Daniela Cassataro <dcassata@gremellab02.ucsd.edu>
2019-04-18 17:31:04 -0700

    bandpass  needs 2018

--- dcTestsBandpass.m
+++ dcTestsBandpass.m
@@ -1,8 +1,5 @@
 % daniela testing stuff
-%% NOTE: you should use the kind with 'fir' as a third argument
-% on ANYTHING large/requiring >13 downsample factor
-% you can also do it in stages, 
-% decY = decimate(y,5,'fir'), decimate that again, etc
+
 %% stuff for all the bandpass stuffs
             
 %            freq_cell = {[1 4], [4 8] , [8 12], [12 30] , [30 50]}
@@ -20,6 +17,17 @@
 %%
 
 freq_cell = {[1 4], [4 8] , [8 12], [12 30] , [30 50]};
+
+
+for i = 1:length(freq_cell)
+    %lowF    = freq_cell{i}(1);
+    %highF   = freq_cell{i}(2);
+    lowAndHigh = freq_cell{i};
+    y = bandpass(exampleSignal,lowAndHigh,30000);
+    
+    
+
+
 
 for i = 1:length(freq_cell) % for each band
     low = freq_cell{i}(1) % this is low
